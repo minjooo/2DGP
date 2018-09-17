81017c2b3826b4a6f4256aeda244f44d890b4a37
minjoo <minf0723@naver.com>
2018-09-17 16:44:02 +0900

    move_circle 완성
    반지름과 각도를 이용해 원 그리기

--- drills/drill05/drill05.py
+++ drills/drill05/drill05.py
@@ -52,27 +52,28 @@ def move_from_right_to_center():
         delay(0.01)
 
 def make_rectangle():
-    # move_from_center_to_right()
-    # move_up()
-    # move_left()
-    # move_down()
-    # move_from_right_to_center()
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
+    move_from_right_to_center()
     pass
 
 def make_circle():
-    angle = 270
-    x,y=800//2,90
-    while angle < 630:
-        x=800//2 + 212 * math.cos(math.radians(angle))
-        y = 300 + 212 * math.sin(math.radians(angle))
-        angle+=1
+    cx, cy ,r =800//2, 600//2, (600-180)//2
+    degree = -90
+    while degree < 270:
+        radian = math.radians(degree)
+        x=cx + r*math.cos(radian)
+        y=cy + r*math.sin(radian)
         clear_canvas_now()
-        grass.draw_now(800//2,30)
-        character.draw_now(x,y)
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        degree += 1
         delay(0.01)
 
 while True:
-    make_rectangle()
+    # make_rectangle()
     make_circle()
 
     
