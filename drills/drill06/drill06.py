0c2460408e0ec6d677fd249635d3cec80f29c28f
minjoo <minf0723@naver.com>
2018-09-20 16:50:42 +0900

    클릭하면 움직이긴 하지만 지나침

--- drills/drill06/drill06.py
+++ drills/drill06/drill06.py
@@ -7,6 +7,8 @@ def handle_events():
     global running
     global hand_x, hand_y
     global x,y
+    global gox,goy
+    global count
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -15,6 +17,10 @@ def handle_events():
             hand_x, hand_y=event.x,KPU_HEIGHT - 1 -event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            count=0
+            gox=x / 50
+            goy=(KPU_HEIGHT - 1 -event.y+50) / 50
     pass
 
 
@@ -26,6 +32,8 @@ hand = load_image('hand_arrow.png')
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 hand_x,hand_y=0,0
+gox,goy=0,0
+count=0
 frame = 0
 hide_cursor()
 
@@ -36,7 +44,10 @@ while running:
     hand.draw(hand_x, hand_y)
     update_canvas()
     frame = (frame + 1) % 8
-
+    if count < 100:
+        count +=2
+        x += gox
+        y += goy
     delay(0.02)
     handle_events()
 
