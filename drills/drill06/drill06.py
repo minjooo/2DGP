10d52401c23db71dd884d21eaf3c3962ac92e13f
minjoo <minf0723@naver.com>
2018-09-20 17:17:36 +0900

    drill06완성re
    2017182006
    https://www.youtube.com/watch?v=mjZLHg49Ck4

--- drills/drill06/drill06.py
+++ drills/drill06/drill06.py
@@ -11,6 +11,7 @@ def handle_events():
     global gox,goy
     global cur_x,cur_y
     global count
+    global where
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -24,6 +25,10 @@ def handle_events():
             gox = (event.x-cur_x-25) / 50
             temp_y = (KPU_HEIGHT - 1 -event.y+35)
             goy=(temp_y-cur_y) / 50
+            if(cur_x>event.x):
+                where=-1
+            elif(cur_x<event.x):
+                where=1
     pass
 
 
@@ -40,12 +45,17 @@ gox,goy=0,0
 count=0
 temp_y=0
 frame = 0
+where=1
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    if where == 1:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+
     hand.draw(hand_x, hand_y)
     update_canvas()
     frame = (frame + 1) % 8
