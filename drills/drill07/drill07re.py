785c959d4614237ae02954b34ad27f041e20da5d
minjoo <minf0723@naver.com>
2018-10-01 17:27:35 +0900

    안된다

--- drills/drill07/drill07re.py
+++ drills/drill07/drill07re.py
@@ -9,42 +9,40 @@ def draw_character(x,y):
         character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     else:
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    frame = (frame + 1) % 8
 
-
-def move_character(p1,p2):
-    for i in range(0, 100 + 1, 1):
-        t=i/100
-        x=(1-t)*p1[0]+t*p2[0]
-        y=(1-t)*p1[1]+t*p2[1]
-
+def move_character (p1, p2):
+    for i in range(0, 100 + 1, 10):
+        t = i/100
+        x = (1-t)*p1[0]+t*p2[0]
+        y = (1-t)*p1[1]+t*p2[1]
+        global frame
+        if where == 1:
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        else:
+            character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        frame = (frame + 1) % 8
+        delay(0.02)
     draw_character(x, y)
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
-running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-cur_x, cur_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-gox,goy=0,0
-count=0
-temp_y=0
 frame = 0
 where=1
 hide_cursor()
-
-
 size=6
 points = [(random.randint(-500,500),random.randint(-350,350)) for i in range(size)]
 n=1
 
-while running:
+while True:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    move_character(points[n-1],points[n])
+    move_character(points[n-1], points[n])
     update_canvas()
-    frame = (frame + 1) % 8
     n = (n + 1) % size
-    delay(0.02)
+
 
 close_canvas()
