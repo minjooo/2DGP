931d2b3eb91e8c786b3d10d1f22c45f8c7598740
minjoo <minf0723@naver.com>
2018-10-04 01:15:31 +0900

    test에 종료 추가

--- project_UPSIDEDOWN/test.py
+++ project_UPSIDEDOWN/test.py
@@ -1,5 +1,14 @@
 from pico2d import *
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type==SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
 open_canvas()
 happy = load_image('run_happiness100.png')
 
@@ -12,3 +21,6 @@ while running == True:
     update_canvas()
     frame=(frame+1)%8
     delay(0.05)
+    handle_events()
+
+close_canvas()
