f5457f336e7af8a5e1c05ca35e23b5036f56dbf8
minjoo <minf0723@naver.com>
2018-10-04 01:03:41 +0900

    왜 안되는지 모르겠다

--- project_UPSIDEDOWN/2.py
+++ project_UPSIDEDOWN/2.py
@@ -1,10 +1,16 @@
 from pico2d import*
 
-happiness = load_image('run_happiness100.png')
-open_canvas(800,600)
+happy = load_image('run_happiness100.png')
+open_canvas()
 
 frame=0
 running=True
 
 while running:
-    pass
+    clear_canvas()
+    happy.clip_draw(frame * 100, 0, 100, 100, 180, 365)
+    update_canvas()
+    frame = (frame + 1)%8
+    delay(0.05)
+
+close_canvas()
