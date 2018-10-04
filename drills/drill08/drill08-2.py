88c28070b67d6a2db91526af3d17c71e6d62b589
minjoo <minf0723@naver.com>
2018-10-04 17:13:23 +0900

    배경을 그리면 캐릭터가 나오지 않음

--- drills/drill08/drill08-2.py
+++ drills/drill08/drill08-2.py
@@ -12,6 +12,13 @@ def handle_events():
             running = False
 
 def draw_character(x,y):
+    global frame
+
+    clear_canvas()
+    #kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame=(frame+1)%8
     pass
 
 def draw_curve_10_point_round(p1,p2,p3,p4):
@@ -34,16 +41,19 @@ def draw_curve_10_point_round(p1,p2,p3,p4):
         y = ((-t**3 + 2*t**2 - t)*p3[1] + (3*t**3 - 5*t**2 + 2)*p4[1] + (-3*t**3 + 4*t**2 + t)*p1[1] + (t**3 - t**2)*p2[1])/2
         draw_character(x, y)
 
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p4[0] + (3*t**3 - 5*t**2 + 2)*p1[0] + (-3*t**3 + 4*t**2 + t)*p2[0] + (t**3 - t**2)*p3[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p4[1] + (3*t**3 - 5*t**2 + 2)*p1[1] + (-3*t**3 + 4*t**2 + t)*p2[1] + (t**3 - t**2)*p3[1])/2
-        draw_character(x, y)
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+character = load_image('animation_sheet.png')
+kpu_ground = load_image('KPU_GROUND.png')
 
 n=1
+frame=0
 points =[(random.randint(-400,400),random.randint(-300,300))for i in range(10)]
 running =True
 
 while running:
     draw_curve_10_point_round(points[n-1],points[n],points[(n+1)%10],points[(n+2)%10])
+    delay(0.05)
     n=(n+1)%10
+    handle_events()
+
+close_canvas()
