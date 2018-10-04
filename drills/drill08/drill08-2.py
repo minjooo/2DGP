daa6d227db36ae3cab3598b5016f50891206681a
minjoo <minf0723@naver.com>
2018-10-04 17:03:49 +0900

    기본틀 완성02

--- drills/drill08/drill08-2.py
+++ drills/drill08/drill08-2.py
@@ -1,36 +1,49 @@
-import turtle
+from pico2d import *
 import random
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
 def draw_character(x,y):
     pass
 
 def draw_curve_10_point_round(p1,p2,p3,p4):
+    global x,y
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
         y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        draw_character((x, y))
+        draw_character(x, y)
 
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p2[0] + (3*t**3 - 5*t**2 + 2)*p3[0] + (-3*t**3 + 4*t**2 + t)*p4[0] + (t**3 - t**2)*p1[0])/2
         y = ((-t**3 + 2*t**2 - t)*p2[1] + (3*t**3 - 5*t**2 + 2)*p3[1] + (-3*t**3 + 4*t**2 + t)*p4[1] + (t**3 - t**2)*p1[1])/2
-        draw_character((x, y))
+        draw_character(x, y)
 
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p3[0] + (3*t**3 - 5*t**2 + 2)*p4[0] + (-3*t**3 + 4*t**2 + t)*p1[0] + (t**3 - t**2)*p2[0])/2
         y = ((-t**3 + 2*t**2 - t)*p3[1] + (3*t**3 - 5*t**2 + 2)*p4[1] + (-3*t**3 + 4*t**2 + t)*p1[1] + (t**3 - t**2)*p2[1])/2
-        draw_character((x, y))
+        draw_character(x, y)
 
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p4[0] + (3*t**3 - 5*t**2 + 2)*p1[0] + (-3*t**3 + 4*t**2 + t)*p2[0] + (t**3 - t**2)*p3[0])/2
         y = ((-t**3 + 2*t**2 - t)*p4[1] + (3*t**3 - 5*t**2 + 2)*p1[1] + (-3*t**3 + 4*t**2 + t)*p2[1] + (t**3 - t**2)*p3[1])/2
-        draw_character((x, y))
+        draw_character(x, y)
 
 n=1
 points =[(random.randint(-400,400),random.randint(-300,300))for i in range(10)]
+running =True
 
-draw_curve_10_point_round(points[n-1],points[n],points[n+1],points[n+2])
-n=(n+1)%10
+while running:
+    draw_curve_10_point_round(points[n-1],points[n],points[(n+1)%10],points[(n+2)%10])
+    n=(n+1)%10
