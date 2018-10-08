4e1c3b297956428a7e05e24caeaad4a1f5f35808
minjoo <minf0723@naver.com>
2018-10-09 01:55:37 +0900

    start 위치조정 및 틀 정리중

--- project_UPSIDEDOWN/UPSIDE_DOWN.py
+++ project_UPSIDEDOWN/UPSIDE_DOWN.py
@@ -2,24 +2,56 @@ from pico2d import *
 
 # 캔버스 크기 900*700
 #게임 오브잭트 클래스 자리
+class Start_Background:
+    def __init__(self):
+        self.image = load_image('start_image.png')
 
+    def draw(self):
+        self.image.draw(450,350)
 
+class Big_start:
+    def __init__(self):
+        self.image = load_image('big_start.png')
+
+    def draw(self):
+        self.image.draw(455,340)
+
+class Small_start:
+    def __init__(self):
+        self.image = load_image('small_start.png')
+
+    def draw(self):
+        self.image.draw(455, 340)
 
 def handle_events():
     global running
+    global starting
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+            starting = False
         elif event.type==SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+            starting = False
 
 # 초키화 자리
 open_canvas(900,700)
+startImage = Start_Background()
+smallStart = Small_start()
+bigStart = Big_start()
 
 running = False
+starting = True
 
 # 메인 루프 자리
+while starting:
+    handle_events()
+
+    clear_canvas()
+    startImage.draw()
+    smallStart.draw()
+    update_canvas()
 
 # 코드 종료 자리
 close_canvas()
