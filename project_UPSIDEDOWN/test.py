d749463b9f61b6bfcef921c8e7d50907b09ebc41
minjoo <minf0723@naver.com>
2018-10-09 04:14:29 +0900

    기쁨이 wait 테스트

--- project_UPSIDEDOWN/test.py
+++ project_UPSIDEDOWN/test.py
@@ -13,6 +13,7 @@ open_canvas()
 happy = load_image('run_happiness100.png')
 sad = load_image('run_sadness100.png')
 Wsad = load_image('wait_sadness200.png')
+Whappy = load_image('wait_happiness200.png')
 
 frame=0
 Wframe=0
@@ -23,6 +24,7 @@ while running == True:
     happy.clip_draw(frame*100,0,100,100,100,100)
     sad.clip_draw(frame*100,0,100,100,230,100)
     Wsad.clip_draw(Wframe*200,0,200,200,500,300)
+    Whappy.clip_draw(Wframe*200,0,200,200,300,500)
     update_canvas()
     frame=(frame+1)%8
     Wframe=(Wframe+1)%4
