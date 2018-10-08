f4d751fe95d2a0ccea3184c77e5d8a6d3b3fd965
minjoo <minf0723@naver.com>
2018-10-08 17:01:33 +0900

    공 떨어진다

--- drills/drill09/drill09.py
+++ drills/drill09/drill09.py
@@ -29,7 +29,10 @@ class Big_Ball:
         self.B_image = load_image('ball41X41.png')
 
     def update(self):
-        self.y -= self.speed
+        if self.y <= 20:
+            pass
+        else:
+            self.y -= self.speed
 
     def draw(self):
         self.B_image.draw(self.x,self.y)
@@ -59,7 +62,11 @@ def handle_events():
 # initialization code
 open_canvas()
 
-team = [Boy() for i in range(11)]
+num = 20
+B_ball = [Big_Ball() for i in range(random.randint(5,15))]
+num = num - len(B_ball)
+S_ball = [Small_Ball() for a in range(num)]
+team = [Boy() for b in range(11)]
 grass = Grass()
 
 running = True
@@ -71,10 +78,20 @@ while running:
     for boy in team:
         boy.update()
 
+    for big in B_ball:
+        big.update()
+
+    for small in S_ball:
+        small.update()
+
     clear_canvas()
     grass.draw()
     for boy in team:
         boy.draw()
+    for big in B_ball:
+        big.draw()
+    for small in S_ball:
+        small.draw()
     update_canvas()
 
     delay(0.05)
