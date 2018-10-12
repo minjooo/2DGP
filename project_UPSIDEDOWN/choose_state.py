2a4c4e36d0804ae31f70c2ab43e8ba54794156b5
minjoo_dt <minf0723@naver.com>
2018-10-13 02:54:45 +0900

    choose 하는중
    왜 안되는지 모르겟다

--- project_UPSIDEDOWN/choose_state.py
+++ project_UPSIDEDOWN/choose_state.py
@@ -1,5 +1,6 @@
 import game_framework
-import choose_state
+import title_state
+import main_state
 from pico2d import *
 
 name = 'choose_state'
@@ -13,76 +14,125 @@ class Cursor:
     def draw(self):
         self.image.draw(self.x, self.y)
 
-class Start_Background:
+class Choose_Background:
     def __init__(self):
-        self.image = load_image('start_image.png')
-
-    def draw(self):
-        self.image.draw(450,350)
-
-class Big_start:
-    def __init__(self):
-        self.image = load_image('big_start.png')
-
-    def draw(self):
-        self.image.draw(450,350)
-
-class Small_start:
-    def __init__(self):
-        self.image = load_image('small_start.png')
+        self.image = load_image('choose.png')
 
     def draw(self):
         self.image.draw(450, 350)
 
+class Wait_happiness:
+    def __init__(self):
+        self.frame = 0
+        self.image = load_image('wait_happiness300.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 4
+
+    def draw(self):
+        self.image.clip_draw(self.frame*300, 0, 300, 300, 300, 300)
+
+class Wait_sadness:
+    def __init__(self):
+        self.frame = 0
+        self.image = load_image('wait_sadness300.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 4
+
+    def draw(self):
+        self.image.clip_draw(self.frame*300, 0, 300, 300, 650, 300)
+
+class Run_happiness300:
+    def __init__(self):
+        self.frame = 0
+        self.image = load_image('run_happiness300.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+
+    def draw(self):
+        self.image.clip_draw(self.frame*300, 0, 300, 300, 300, 300)
+
+class Run_sadness300:
+    def __init__(self):
+        self.frame = 2
+        self.image = load_image('run_sadness300.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 300, 0, 300, 300, 650, 300)
+
 
 def enter():
-    global start_select, cursor, start_bg, big_start, small_start
-    start_select = False
+    global cursor, choose_happy, choose_sad, choose_bg, wait_sad, wait_happy, run_sad, run_happy
     cursor = Cursor()
-    start_bg = Start_Background()
-    big_start = Big_start()
-    small_start = Small_start()
+    choose_happy = False
+    choose_sad = False
+    choose_bg = Choose_Background()
+    wait_sad = Wait_sadness()
+    wait_happy = Wait_happiness()
+    run_sad = Run_sadness300()
+    run_happy = Run_happiness300()
+
 
 def exit():
-    global start_select, cursor, start_bg, big_start, small_start
+    global cursor, choose_happy, choose_sad, choose_bg, wait_sad, wait_happy, run_sad, run_happy
     del(cursor)
-    del(start_select)
-    del(start_bg)
-    del(big_start)
-    del(small_start)
+    del(choose_happy)
+    del(choose_sad)
+    del(choose_bg)
+    del(wait_sad)
+    del(wait_happy)
+    del(run_sad)
+    del(run_happy)
 
 def handle_events():
-    global start_select
+    global cursor
+    global choose_happy
+    global choose_sad
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_MOUSEMOTION:
             cursor.x, cursor.y = event.x, 700 - 1 - event.y
-            if starting:
-                if 415 < event.x < 695 and 150 < (700 - 1 - event.y) < 260:
-                    start_select = True
-                else:
-                    start_select = False
+            if 150 < event.x < 450 and 100 < (700 - 1 - event.y) < 450:
+                choose_happy = True
+            else:
+                choose_happy = False
+            if 500 < event.x < 800 and 100 < (700 - 1 - event.y) < 450:
+                choose_sad = True
+            else:
+                choose_sad = False
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-            if start_select:
-                game_framework.change_state(choose_state)
+            if choose_happy:
+                pass
+            elif choose_sad:
+                pass
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
-                gmae_framework.quit()
-            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-                game_framework.change_state(choose_state) #  일단 넣어놓은것 나중에 빼자
+                game_framework.change_state(title_state)
 
 def update():
-    pass
+    Wait_happiness.update()
+    Wait_sadness.update()
+    Run_happiness300.update()
+    Run_sadness300.update()
 
 def draw():
     clear_canvas()
-    start_bg.draw()
-    if start_select:
-        big_start.draw()
+    choose_bg.draw()
+    if choose_happy:
+        run_happy.draw()
     else:
-        small_start.draw()
+        wait_happy.draw()
+    if choose_sad:
+        run_sad.draw()
+    else:
+        wait_sad.draw()
     cursor.draw()
     update_canvas()
 
--- project_UPSIDEDOWN/title_state.py
+++ project_UPSIDEDOWN/title_state.py
@@ -71,8 +71,6 @@ def handle_events():
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
-            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-                game_framework.change_state(choose_state) #  일단 넣어놓은것 나중에 빼자
 
 def update():
     pass
