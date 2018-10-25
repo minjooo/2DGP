0b9cc9ee92646def09c47e181ace876eaf66f586
minjoo <minf0723@naver.com>
2018-10-25 21:50:19 +0900

    drill11완성
    2017182006 김민주
    커밋횟수 : 1회
    https://www.youtube.com/watch?v=mGaL0E5GGeA

--- drills/drill11/main_state.py
+++ drills/drill11/main_state.py
@@ -1,64 +1,34 @@
 import random
-import pause_state
 import json
 import os
 
 from pico2d import *
-
 import game_framework
-import title_state
+import game_world
 
+from boy import Boy
+from grass import Grass
+from ball import Ball
 
 
 name = "MainState"
 
 boy = None
 grass = None
-font = None
-
-class Grass:
-    def __init__(self):
-        self.image = load_image('grass.png')
-
-    def draw(self):
-        self.image.draw(400, 30)
-
-
-
-class Boy:
-    def __init__(self):
-        self.x, self.y = 0, 90
-        self.frame = 0
-        self.image = load_image('run_animation.png')
-        self.dir = 1
-
-    def update(self):
-        self.frame = (self.frame + 1) % 8
-        self.x += self.dir
-        if self.x >= 800:
-            self.dir = -1
-        elif self.x <= 0:
-            self.dir = 1
-
-    def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
-
 
 def enter():
     global boy, grass
     boy = Boy()
     grass = Grass()
+    game_world.add_object(grass, 0)
+    game_world.add_object(boy, 1)
 
 
 def exit():
-    global boy, grass
-    del(boy)
-    del(grass)
-
+    game_world.clear()
 
 def pause():
     pass
-
 
 
 def resume():
@@ -66,26 +36,28 @@ def resume():
 
 
 def handle_events():
-    global pause
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            game_framework.change_state(title_state)
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            game_framework.push_state(pause_state)
+                game_framework.quit()
+        else:
+            boy.handle_event(event)
 
 
 def update():
-    boy.update()
+    for game_object in game_world.all_objects():
+        game_object.update()
 
 
 def draw():
     clear_canvas()
-    grass.draw()
-    boy.draw()
+    for game_object in game_world.all_objects():
+        game_object.draw()
     update_canvas()
+
+
 
 
 
