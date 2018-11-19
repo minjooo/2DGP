bede2e56aefe533174fbd8f5b1f0755756a3c3aa
minjoo <minf0723@naver.com>
2018-11-20 02:04:34 +0900

    기쁨이도 바꿨다

--- project_UPSIDEDOWN/Run_happiness100.py
+++ project_UPSIDEDOWN/Run_happiness100.py
@@ -1,42 +1,149 @@
 from pico2d import *
 
-import game_world
+SPACE, DELETE, LANDING = range(3)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_DELETE): DELETE,
+    (SDL_KEYDOWN, SDLK_SPACE): SPACE
+}
+
+
+class RunUpState:
+
+    @staticmethod
+    def enter(happy, event):
+        pass
+
+    @staticmethod
+    def exit(happy, event):
+        pass
+
+    @staticmethod
+    def do(happy):
+        happy.frame = (happy.frame + 1) % 8
+
+    @staticmethod
+    def draw(happy):
+        happy.image.clip_draw(happy.frame * 100, 0, 100, 100, 200, 360)
+
+
+class RunDownState:
+
+    @staticmethod
+    def enter(happy, event):
+        pass
+
+    @staticmethod
+    def exit(happy, event):
+        pass
+
+    @staticmethod
+    def do(happy):
+        happy.frame = (happy.frame + 1) % 8
+
+    @staticmethod
+    def draw(happy):
+        happy.image.clip_composite_draw(happy.frame * 100, 0, 100, 100, 0, 'v', 200, 240, 100, 100)
+
+
+class JumpUpState:
+
+    @staticmethod
+    def enter(happy, event):
+        pass
+
+    @staticmethod
+    def exit(happy, event):
+        pass
+
+    @staticmethod
+    def do(happy):
+        if happy.goup:
+            happy.height += happy.jump_speed[happy.count_jump_speed]
+            happy.count_jump_speed -= 1
+            if happy.count_jump_speed == -8:
+                happy.goup = False
+        if happy.goup == False:
+            happy.height -= happy.jump_speed[happy.count_jump_speed]
+            happy.count_jump_speed += 1
+            if happy.count_jump_speed == 1:
+                happy.goup = True
+                happy.jump = False
+                happy.count_jump_speed = -1
+                happy.add_event(LANDING)
+
+    @staticmethod
+    def draw(happy):
+        happy.image_jump.draw(200, 360 + happy.height)
+
+
+class JumpDownState:
+
+    @staticmethod
+    def enter(happy, event):
+        pass
+
+    @staticmethod
+    def exit(happy, event):
+        pass
+
+    @staticmethod
+    def do(happy):
+        if happy.goup:
+            happy.height += happy.jump_speed[happy.count_jump_speed]
+            happy.count_jump_speed -= 1
+            if happy.count_jump_speed == -8:
+                happy.goup = False
+        if happy.goup == False:
+            happy.height -= happy.jump_speed[happy.count_jump_speed]
+            happy.count_jump_speed += 1
+            if happy.count_jump_speed == 1:
+                happy.goup = True
+                happy.jump = False
+                happy.count_jump_speed = -1
+                happy.add_event(LANDING)
+
+    @staticmethod
+    def draw(happy):
+        happy.image_jump.clip_composite_draw(0, 0, 100, 100, 0, 'v', 200, 240 - happy.height, 100, 100)
+
+
+next_state_table = {
+    RunUpState: {SPACE: JumpUpState, DELETE: RunDownState},
+    RunDownState: {SPACE: JumpDownState, DELETE: RunUpState},
+    JumpUpState: {SPACE: JumpUpState, DELETE: JumpUpState, LANDING: RunUpState},
+    JumpDownState: {SPACE: JumpDownState, DELETE: JumpDownState, LANDING: RunDownState}
+}
+
 
 class Run_happiness100:
     def __init__(self):
-        self.point = 0
-        self.UP = True
-        self.height = 0
-        self.jump = False
-        self.goup = True # 점프할 때 올라가는지 내려가는지 구분
+        self.frame = 0
+        self.goup = True
         self.jump_speed = [n for n in range(0, 35 + 1) if n % 5 == 0]
         self.count_jump_speed = -1
-        self.frame = 0
+        self.height = 0
         self.image = load_image('resources\\run_happiness100.png')
         self.image_jump = load_image('resources\\run_happiness100_jump.png')
+        self.event_que = []
+        self.cur_state = RunUpState
+        self.cur_state.enter(self, None)
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
 
     def update(self):
-        self.frame = (self.frame + 1) % 8
-        if self.jump:
-            if self.goup:
-                self.height += self.jump_speed[self.count_jump_speed]
-                self.count_jump_speed -= 1
-                if self.count_jump_speed == -8:
-                    self.goup = False
-            if self.goup == False:
-                self.height -= self.jump_speed[self.count_jump_speed]
-                self.count_jump_speed += 1
-                if self.count_jump_speed == 1:
-                    self.goup = True
-                    self.jump = False
-                    self.count_jump_speed = -1
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
 
-    def draw_up(self):
-        self.image.clip_draw(self.frame * 100 , 0, 100, 100, 200, 360)
-    def draw_down(self):
-        self.image.clip_composite_draw(self.frame * 100, 0, 100, 100, 0, 'v', 200, 240, 100, 100)
-    def draw_jump_up(self):
-        self.image_jump.draw(200, 360 + self.height)
-    def draw_jump_down(self):
-        self.image_jump.clip_composite_draw(0, 0, 100, 100, 0, 'v', 200, 240 - self.height, 100, 100)
+    def draw(self):
+        self.cur_state.draw(self)
 
+    def handle_event(self, event):
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
--- project_UPSIDEDOWN/main_state.py
+++ project_UPSIDEDOWN/main_state.py
@@ -30,25 +30,26 @@ class Running_Background:
 
 
 def enter():
-    global main_bg, run_happy, run_sad, path, number, following_marbles, is_crush, map
+    global main_bg, character, path, number, following_marbles, is_crush, map
     main_bg = Running_Background()
-    run_happy = Run_happiness100()
-    run_sad = Run_sadness100()
+    if choose_state.selected_character == 'sad':
+        character = Run_sadness100()
+    elif choose_state.selected_character == 'happy':
+        character = Run_happiness100()
     path = Path()
     number = Numbers()
     map = Map()
     following_marbles = FollowingMarbles()
     is_crush = False
     game_world.add_object(map, 0)
-    game_world.add_object(run_sad, 0)
+    game_world.add_object(character, 0)
 
 
 
 def exit():
-    global main_bg, run_happy, run_sad, path, number, Cards_up, Boyfriends_up, Brooms_up, Marbles_up,Cards_down, Boyfriends_down, Brooms_down, Marbles_down, Tray_up, Tray_down, up, down,following_marbles,  is_crush
+    global main_bg, character, path, number, Cards_up, Boyfriends_up, Brooms_up, Marbles_up,Cards_down, Boyfriends_down, Brooms_down, Marbles_down, Tray_up, Tray_down, up, down,following_marbles,  is_crush
     del main_bg
-    del run_happy
-    del run_sad
+    del character
     del path
     del number
     del following_marbles
@@ -93,7 +94,7 @@ def draw():
         game_object.draw()
     if following_marbles.order != 0:
         if choose_state.selected_character == 'sad':
-            if run_happy.UP:
+            if character.UP:
                 for following_marbles.num in range(1, following_marbles.order + 1):
                     following_marbles.blue_draw_up_f()
             else:
@@ -101,7 +102,7 @@ def draw():
                     following_marbles.blue_draw_down_f()
 
         elif choose_state.selected_character == 'happy':
-            if run_happy.UP:
+            if character.UP:
                 for following_marbles.num in range(1, following_marbles.order + 1):
                     following_marbles.yellow_draw_up_f()
             else:
@@ -139,54 +140,54 @@ def check_Crush():
         else:
             i.check = False
 
-    if run_happy.UP == True or run_sad.UP == True:
+    if character.UP == True or character.UP == True:
         for i in ups:  # 충돌 계산 해줄곳 위
             if i.check == True:
                 if type(i) == Card:
-                    is_crush = collide(170, 220, 360 + run_happy.height - 10, 360 + run_happy.height + 40,
+                    is_crush = collide(170, 220, 360 + character.height - 10, 360 + character.height + 40,
                                        i.x - 25, i.x + 25, 310, 370)
                 if type(i) == Boyfriend:
-                    is_crush = collide(170, 220, 360 + run_happy.height - 10, 360 + run_happy.height + 40,
+                    is_crush = collide(170, 220, 360 + character.height - 10, 360 + character.height + 40,
                                        i.x - 25, i.x + 25, 310, 510)
                 if type(i) == Broom:
-                    is_crush = collide(170, 220, 360 + run_happy.height - 10, 360 + run_happy.height + 40,
+                    is_crush = collide(170, 220, 360 + character.height - 10, 360 + character.height + 40,
                                        i.x - 20, i.x + 20, 419, 610)
                 if type(i) == Marble:
                     if i.eated == False:
-                        i.eated = collide(170, 220, 360 + run_happy.height - 10, 360 + run_happy.height + 40,
+                        i.eated = collide(170, 220, 360 + character.height - 10, 360 + character.height + 40,
                                           i.x - 15, i.x + 15, 310, 350)
                         if i.eated == True:
                             number.marble_number += 1
                 if type(i) == Tray:
                     if i.full == False:
-                        i.full = collide(170, 220, 360 + run_happy.height - 10, 360 + run_happy.height + 40,
+                        i.full = collide(170, 220, 360 + character.height - 10, 360 + character.height + 40,
                                          i.x - 30, i.x + 30, 310, 400)
                         if i.full == True:
                             number.score = number.marble_number*1000
                             number.marble_number = 0
 
 
-    if run_happy.UP == False or run_sad.UP == False:
+    if character.UP == False or character.UP == False:
         for i in downs:  # 충돌 계산 해줄곳 아래
             if i.check == True:
                 if type(i) == Card:
-                    is_crush = collide(170, 220, 240 - run_happy.height - 40, 240 - run_happy.height + 10,
+                    is_crush = collide(170, 220, 240 - character.height - 40, 240 - character.height + 10,
                                        i.x - 25, i.x + 25, 230, 290)
                 if type(i) == Boyfriend:
-                    is_crush = collide(170, 220, 240 - run_happy.height - 40, 240 - run_happy.height + 10,
+                    is_crush = collide(170, 220, 240 - character.height - 40, 240 - character.height + 10,
                                        i.x - 25, i.x + 25, 140, 290)
                 if type(i) == Broom:
-                    is_crush = collide(170, 220, 240 - run_happy.height - 40, 240 - run_happy.height + 10,
+                    is_crush = collide(170, 220, 240 - character.height - 40, 240 - character.height + 10,
                                        i.x - 20, i.x + 20, 0, 191)
                 if type(i) == Marble:
                     if i.eated == False:
-                        i.eated = collide(170, 220, 240 - run_happy.height - 40, 240 - run_happy.height + 10,
+                        i.eated = collide(170, 220, 240 - character.height - 40, 240 - character.height + 10,
                                           i.x - 15, i.x + 15, 250, 290)
                         if i.eated == True:
                             number.marble_number += 1
                 if type(i) == Tray:
                     if i.full == False:
-                        i.full = collide(170, 220, 240 - run_happy.height - 40, 240 - run_happy.height + 10,
+                        i.full = collide(170, 220, 240 - character.height - 40, 240 - character.height + 10,
                                          i.x - 30, i.x + 30, 200, 290)
                         if i.full == True:
                             number.score = number.marble_number*1000
